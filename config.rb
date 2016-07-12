0b96bf8471422503dc1d88c1a68fe02998493f8c
stuartmoffatt <stuart@magnoliabox.com>
2016-07-12 12:02:11 +0100

    Inline our core CSS for better perf

--- config.rb
+++ config.rb
@@ -4,11 +4,22 @@ set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
+
 configure :build do
-  activate :minify_css
+  activate :minify_css, :inline => true
   activate :minify_html
   # Ignore favicons and photos.css (that we lazy load, so need filename to stay)
   activate :asset_hash, :ignore => [/([^\/]+\.png)$/, /css\/photos\.css$/]
+end
+
+helpers do
+  def inline_css(*names)
+    names.map { |name|
+      name += ".css" unless name.include?(".css")
+      css_path = sitemap.resources.select { |p| p.source_file.include?(name) }.first
+      "<style type='text/css'>#{css_path.render}</style>"
+    }.reduce(:+)
+  end
 end
 
 activate :deploy do |deploy|
